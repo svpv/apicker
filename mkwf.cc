1be5b28a171e44db2c76e04f86c10fab0008f58c
Alexey Tourbin <alexey.tourbin@gmail.com>
2015-06-14 16:20:22 +0300

    mkwf: automatically generate in.mp3.wf output filename

--- mkwf.cc
+++ mkwf.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "waveformgen.h"
 
 int main(int argc, char *argv[])
@@ -6,7 +7,13 @@ int main(int argc, char *argv[])
     try {
 	WaveformGen gen(argv[1]);
 	gen.loop();
-	gen.save(argv[2]);
+	if (argv[2])
+	    gen.save(argv[2]);
+	else {
+	    std::string out(argv[1]);
+	    out.append(".wf");
+	    gen.save(out.c_str());
+	}
     }
     catch (const std::exception &e) {
 	std::cerr << e.what() << std::endl;
