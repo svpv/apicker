8a9d6018f710a86472adc8a27012d79c12474582
Alexey Tourbin <alexey.tourbin@gmail.com>
2015-06-16 16:12:54 +0300

    waveformview: scroll akin to overviewbar

--- waveformview.h
+++ waveformview.h
@@ -8,18 +8,16 @@
 class WaveformView : public Gtk::DrawingArea
 {
 public:
-    WaveformView(Waveform *wf, Glib::RefPtr<Gtk::Adjustment> aj)
-	: m_wf(wf), m_aj(aj)
+    WaveformView(Waveform *wf, Glib::RefPtr<Gtk::Adjustment> aj) :
+	m_wf(wf), m_aj(aj),
+	m_cursor_x_centered(0), m_cursor_x_current(0), m_cursor_x_from_click(-9)
     {
 	set_size_request(1200, 280);
+	add_events(Gdk::BUTTON_PRESS_MASK | Gdk::BUTTON_RELEASE_MASK | Gdk::POINTER_MOTION_MASK);
 	m_aj->signal_value_changed().connect(
 		sigc::mem_fun(*this, &WaveformView::queue_draw));
     }
-
-    ~WaveformView()
-    {
-    }
-
+protected:
     bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
     {
 	size_t w = get_width();
@@ -54,6 +52,20 @@ public:
 	    cursor_x = w - (m_wf->m_n - wf_c);
 	}
 
+	// cursor should be displayed here
+	m_cursor_x_centered = cursor_x;
+
+	// however, when the user clicks, the new position is centered gradually
+	if (m_cursor_x_from_click > -9) {
+	    cursor_x = m_cursor_x_from_click;
+	    wf_x = wf_c - cursor_x;
+	    if (wf_x > wf_c)
+		wf_x = 0;
+	}
+
+	// cursor is displayed here
+	m_cursor_x_current = cursor_x;
+
 	// draw second marks
 	size_t r = wf_x % 100;
 	for (size_t i = r ? 100 - r : 0; i < w; i += 100) {
@@ -81,9 +93,78 @@ public:
 
 	return true;
     }
-protected:
+
+    void click(double x)
+    {
+	const size_t w = get_width();
+
+	// bound x
+	if (x < 0) x = 0;
+	if (x > w) x = w;
+
+	// idicate cursor_x position from click
+	m_cursor_x_from_click = x;
+
+	// set new position in the stream
+	double diff = x - m_cursor_x_current;
+	m_aj->set_value(m_aj->get_value() + diff);
+
+	queue_draw();
+    }
+
+    bool on_button_press_event(GdkEventButton *event) override
+    {
+	if (m_tick)
+	    m_tick.block();
+	click(event->x);
+	m_drag = true;
+	return true;
+    }
+
+    bool on_button_release_event(GdkEventButton *event) override
+    {
+	m_drag = false;
+	if (m_tick)
+	    m_tick.unblock();
+	else
+	    m_tick = Glib::signal_timeout().connect(
+		    sigc::mem_fun(*this, &WaveformView::tick), 10);
+	return true;
+    }
+
+    bool on_motion_notify_event(GdkEventMotion* event) override
+    {
+	if (m_drag)
+	    click(event->x);
+	return true;
+    }
+
+    bool tick()
+    {
+	double diff = m_cursor_x_current - m_cursor_x_centered;
+	if (fabs(diff) < 5) {
+	    m_cursor_x_from_click = -9;
+	    m_tick.block();
+	    goto out;
+	}
+	if (diff > 0)
+	    diff -= 4;
+	else
+	    diff += 4;
+	diff *= 0.99;
+	m_cursor_x_from_click = m_cursor_x_centered + diff;
+    out:
+	queue_draw();
+	return true;
+    }
+
     Waveform *m_wf;
     Glib::RefPtr<Gtk::Adjustment> m_aj;
+    double m_cursor_x_centered;
+    double m_cursor_x_current;
+    double m_cursor_x_from_click;
+    sigc::connection m_tick;
+    bool m_drag;
 };
 
 #endif
