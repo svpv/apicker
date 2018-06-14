2bba0a78e05555ad4c15aebe3a413945b384fd77
Alexey Tourbin <alexey.tourbin@gmail.com>
2018-06-14 04:00:20 +0300

    rms.h: calculate RMS properly
    All samples should be squared as is, instead of taking average between
    the channels.  (This can be slightly slower; on the other hand it can be
    vectorized; I have not looked into this yet.)  The result is no longer
    adjusted for the number of channels.  It does not make much difference,
    because in waveformgen.h, levels are calibrated against q95 and q999.

--- rms.h
+++ rms.h
@@ -5,55 +5,52 @@ class RMS: public AReader
 {
 public:
     RMS(const char *fname)
-	: AReader(fname), m_sum(0), m_nsum(0)
+	: AReader(fname), m_sumsq(0), m_nsum(0)
     {
 	m_nmax = rate() / 100; // 10 msec
     }
 private:
     template<typename T>
-    inline double chsum(T  *data, size_t i, size_t nc)
+    inline double sumsq(T  *data, size_t i, size_t nc)
     {
-	double s = data[i * nc];
-	for (size_t j = 1; j < nc; j++)
-	    s += data[i * nc + j];
+	double s = 0;
+	for (size_t j = 0; j < nc; j++)
+	    s += data[i * nc + j] *
+		 data[i * nc + j] ;
 	return s;
     }
     template<typename T>
-    inline double chsum(T **data, size_t i, size_t nc)
+    inline double sumsq(T **data, size_t i, size_t nc)
     {
-	double s = data[0][i];
-	for (size_t j = 1; j < nc; j++)
-	    s += data[j][i];
+	double s = 0;
+	for (size_t j = 0; j < nc; j++)
+	    s += data[j][i] *
+		 data[j][i] ;
 	return s;
     }
-    inline double avg(short  *data, size_t i, size_t nc) { return chsum(data, i, nc) / (nc * 32767); }
-    inline double avg(short **data, size_t i, size_t nc) { return chsum(data, i, nc) / (nc * 32767); }
-    inline double avg(float  *data, size_t i, size_t nc) { return chsum(data, i, nc) / nc; }
-    inline double avg(float **data, size_t i, size_t nc) { return chsum(data, i, nc) / nc; }
+    double range(short  *data) { return 32767; }
+    double range(short **data) { return 32767; }
+    double range(float  *data) { return 1; }
+    double range(float **data) { return 1; }
 
-    double m_sum;
+    double m_sumsq;
     size_t m_nsum;
     size_t m_nmax;
     std::vector<double> m_rms;
 
-    inline void acc(double avg)
-    {
-	m_sum += avg * avg;
-	m_nsum++;
-	if (m_nsum < m_nmax)
-	    return;
-
-	double rms = sqrt(m_sum / m_nmax);
-	m_rms.push_back(rms);
-	m_sum = 0;
-	m_nsum = 0;
-    }
-
     template<typename T>
     void do_process(T data, size_t n, size_t nc)
     {
-	for (size_t i = 0; i < n; i++)
-	    acc(avg(data, i, nc));
+	for (size_t i = 0; i < n; i++) {
+	    m_sumsq += sumsq(data, i, nc);
+	    m_nsum++;
+	    if (m_nsum < m_nmax)
+		continue;
+	    double rms = sqrt(m_sumsq / (m_nsum * range(data) * range(data)));
+	    m_rms.push_back(rms);
+	    m_sumsq = 0;
+	    m_nsum = 0;
+	}
     }
 
     template<typename T>
@@ -68,7 +65,7 @@ private:
 	    do_process(data, n, 2);
 	    break;
 	default:
-	    throw "unsupported number of channels";
+	    do_process(data, n, nc);
 	}
     }
 protected:
